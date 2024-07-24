bd8f7af4b672bd160d8ec2a88bf4f8f9289aa2cc
lidaqiang <dqli161@163.com>
2024-07-24 13:57:31 +0800

    fix comment

--- test/0.7.0/Counter.ts
+++ test/0.7.0/Counter.ts
@@ -6,7 +6,7 @@ describe('Counter', () => {
     it ('Get admin address', async () => {
         await deployments.fixture(['Counter']) // deploy Counter contract first
 
-        const Admin = await ethers.getContract('Admin') as Contract // get admin deployment
+        const Admin = await ethers.getContract('Admin') as Contract // interact with deployed Admin contract
         const Counter = await ethers.getContract('Counter') as Contract // interact with deployed Counter contract
 
         expect(Counter.ADMIN_ADDRESS).to.equal(Admin.address)
@@ -14,8 +14,7 @@ describe('Counter', () => {
     it ('Add count', async () => {
         await deployments.fixture(['Counter']) // deploy Counter contract first
 
-        const Admin = await ethers.getContract('Admin') as Contract // get admin deployment
-
+        const Admin = await ethers.getContract('Admin') as Contract // interact with deployed Admin contract
         const Counter = await ethers.getContract('Counter') as Contract // interact with deployed Counter contract
 
         await Admin.add() // increase count
