6c9edffccca021a224d0edd621bbe2a7bfccfe6b
lidaqiang <dqli161@163.com>
2024-07-24 13:56:38 +0800

    update

--- deploy/100_aggregate_all_contract.ts
+++ deploy/100_aggregate_all_contract.ts
@@ -1,16 +1,23 @@
-import {HardhatRuntimeEnvironment} from 'hardhat/types';
-import {DeployFunction, Deployment} from 'hardhat-deploy/types'; 
+import {HardhatRuntimeEnvironment} from 'hardhat/types'
+import {DeployFunction, Deployment} from 'hardhat-deploy/types' 
 
 const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-    const { deployments } = hre; 
+    const { deployments } = hre 
     const _allDeployments = await deployments.all()
     const allDeployments = new Map<string, Deployment>(Object.entries(_allDeployments))
-    console.log('all contract: ')
-    allDeployments.forEach((value, key) => {
-        console.log(`${key}: ${value.address}`);
-      });
-};
 
-export default func;
-func.tags = ['Aggregate']; 
-func.runAtTheEnd = true;
+    interface MyContract {
+      ContractName: string
+      Address: string
+    }
+
+    const contracts = [] as Array<MyContract>
+    allDeployments.forEach((value, key) => {
+        contracts.push({ContractName: key, Address: value.address} as MyContract)
+    })
+    console.log(contracts)
+}
+
+export default func
+func.tags = ['Aggregate'] 
+func.runAtTheEnd = true
