7a985bea377c5fd2388f3775508c9e24698f64b4
lidaqiang <dqli161@163.com>
2024-08-02 18:56:00 +0800

    delete slot in script

--- contracts/impl/0.8.20/Counter.sol
+++ contracts/impl/0.8.20/Counter.sol
@@ -4,6 +4,7 @@ pragma solidity =0.8.20;
 import { Initializable } from '@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol';
 import { UUPSUpgradeable } from '@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol';
 import { OwnableUpgradeable } from '@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol';
+import { ERC1967Utils } from '@openzeppelin/contracts/proxy/ERC1967/ERC1967Utils.sol';
 
 contract Counter is Initializable, UUPSUpgradeable, OwnableUpgradeable {
     uint256 public count;
@@ -65,5 +66,9 @@ contract Counter is Initializable, UUPSUpgradeable, OwnableUpgradeable {
 
     function getAdminAddress() public view returns (address) {
         return s_adminContractAddress;
+    }
+
+    function getImplementation() public view returns (address) {
+        return ERC1967Utils.getImplementation();
     }
 }
